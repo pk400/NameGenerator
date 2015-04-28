1346d18743e92502874826e5a014be7e748a3931
Joel Aro <jaromail04@gmail.com>
2015-04-27 22:15:33 -0400

    Main program process

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import java.util.Random;
+
 
 public class Main {
-
+	static String[] fname = { "Joel", "Joshua", "Magic", "Kevin", "Thomas", "Francis"};
+	static String[] lname = { "Aro", "Alcoran", "Tan", "Tran", "Balani", "Mora", "Nguyen"};
+	
+	public static void main(String args[]) {
+		Random randFName = new Random();
+		Random randLName = new Random();
+		
+		int rfn = randFName.nextInt(5) + 0;
+		int rln = randLName.nextInt(6) + 0;
+		
+		System.out.println(fname[rfn] + " " + lname[rln]);
+	}
 }
